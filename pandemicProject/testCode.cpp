2285c460a6e42662f08b2afbc18b20135e131363
Howsterz <duralc1990@gmail.com>
2013-10-01 22:36:17 -0700

    Still testing how to completely commit from VS with a project.

--- pandemicProject/testCode.cpp
+++ pandemicProject/testCode.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "conio.h"
+
 using namespace std;
 
 int main() {
-	cout << "Hello World! This is a test .cpp file! :)" << endl;
+	cout << "This is a test!" << endl;
 	_getch();
 	return 0;
 }
