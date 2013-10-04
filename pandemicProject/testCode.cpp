5229695af923e604b5090b2c72bdb3ed52a426f6
dgoostree <darrengoostree@gmail.com>
2013-10-04 13:44:30 -0700

    Testing

--- pandemicProject/testCode.cpp
+++ pandemicProject/testCode.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main() {
 	cout << "This is a test! And now I made changes. Does git recognize this?" << endl;
+	cout << "This is also a test! I've made additional changes... is this thing working?" << endl;
 	_getch();
 	return 0;
 }
