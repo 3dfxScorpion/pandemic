87a90bc82f4c9b4cf2648c4b26d6e03719649c2b
U-cobra\dbuck <dbuck@cobra.(none)>
2013-10-04 18:30:17 -0700

    modifies testCode.cpp to be more "platform friendly" commented out conio.h and _getch() and replaced with cin.get() to achieve the same pause for key press after run.

--- pandemicProject/testCode.cpp
+++ pandemicProject/testCode.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include "conio.h"
+// #include "conio.h"  // does not compile on command line --Derrick
 
 using namespace std;
 
 int main() {
 	cout << "This is a test! And now I made changes. Does git recognize this?" << endl;
 	cout << "This is also a test! I've made additional changes... is this thing working?" << endl;
-	_getch();
+//	_getch();  // dependent on conio.h  --Derrick
+  cin.get();   // replacement for _getch()  --Derrick
 	return 0;
 }
