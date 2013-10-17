687def865c5c16fde96aa352935c73314272a125
ErrantSpore <don.s.umble@gmail.com>
2013-10-17 14:23:11 -0700

    Removing unnecessary files

--- pandemicProject/Map_City_Class/Tester.cpp
+++ pandemicProject/Map_City_Class/Tester.cpp
@@ -5,19 +5,24 @@ using namespace std;
 
 int main() {
 	mapClass gameMap;
-	string filename = "Cities_test.txt";
+	string filename = "Cities_test.txt";	// name of file containing map information
 
-	cout << gameMap.getNumberofCities() << endl;
-
+	cout << gameMap.getNumberofCities() << endl;			// outputs number of cities in map before populate function
+	
+	cout << "Populating map from file." << endl;
 	gameMap.populateMap(filename);
+	cout << "Finished populating map."  << endl;
 
-	cout << gameMap.getNumberofCities() << endl << endl;;
+	cout << gameMap.getNumberofCities() << endl << endl;;	// outputs number of cities in map after populate function
 
+/*
+	// Used for testing setting infected levels and function for checking if isInfected changes properly
 	gameMap.worldMap[3].setInfectedBlack(0);
 	gameMap.worldMap[3].setInfectedBlue(0);
 	gameMap.worldMap[3].setInfectedRed(0);
 	gameMap.worldMap[3].setInfectedYellow(0);
-
+*/
+	// outputs cities in map - how to access information contained in each city
 	for(int i = 0; i < gameMap.getNumberofCities(); i++) {
 		cout << "City:   " << gameMap.worldMap[i].getCityName()			<< endl;
 		cout << "Color:  " << gameMap.worldMap[i].getCityColor()		<< endl;
@@ -37,11 +42,13 @@ int main() {
 	}
 
 /*
+	// test of searching map for city and returning city info
 	cityClass currentCity = gameMap.locateCity("Essen");
 	cout << currentCity.getCityName() << endl;
 */
-
-	cout << gameMap.infectedList();
+	
+	// test of infectedList formatted output
+	cout << endl << gameMap.infectedList() << endl;
 	
 	cout << "Press any key to exit.";
 	cin.get();
