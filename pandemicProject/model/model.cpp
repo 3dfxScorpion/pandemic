09be8030c382297eaabafe64a1d3f30e083449c0
dgoostree <darrengoostree@gmail.com>
2013-10-16 19:22:24 -0700

    Adding beginning integration

---
+++ pandemicProject/model/model.cpp
@@ -0,0 +1,39 @@
+//
+// This is a temporary holder for data that will be in the model (similar to the "board" superclass we discussed) class after we get far enough to implement MVC.
+// For now, various stuff belonging to View/controller will also be here for the submission
+/*#include <iostream>
+using namespace std;
+#include "temp\Player.h"
+//#include "temp\cityClass.h"
+
+
+int main()
+{
+	int numPlayers = 0;										// stores player count
+	int outbreak = 0;										// initial outbreak value
+	int infRate[7] = {2, 2, 2, 3, 3, 4, 4};					//represents infection rate on the board
+	int currentRate = 0;									// starting infection level offset
+	int redCubes = 24;										//initial unused cube count
+	int yellowCubes = 24;
+	int blueCubes = 24;
+	int blackCubes = 24;
+	Player players[4];
+
+	cout << "Welcome to pandemic." << endl;
+	cout << "Enter the number of players: ";
+	cin >> numPlayers;
+
+	for (int i = 0; i < numPlayers; i++)
+	{
+		string temp;
+		cout << "Please enter player " << i+1 << "'s name: ";
+		cin >> temp; 
+		players[i].setPlayerName(temp);
+		cout << players[i].getPlayerName() << endl;
+	}
+
+	cout << "Done.";
+
+	return 0;
+}
+*/
