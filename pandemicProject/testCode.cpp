cc245135756263940bf0c58eef793d3d4cd7cdeb
Howsterz <duralc1990@gmail.com>
2013-10-16 21:25:20 -0700

    Removed some files that weren't needed.

--- pandemicProject/testCode.cpp
+++ pandemicProject/testCode.cpp
@@ -1,15 +1,13 @@
 //Use this to test any code you may have written. This will be replaced with a real main function once we get around to it.
 
-/*#include <iostream>
+#include <iostream>
 #include <string>
-//#include "CityCard.h"
-
 using namespace std;
 
 int main() {
 	cout << "This is a test! And now I made changes. Does git recognize this?" << endl;
 	cout << "This is also a test! I've made additional changes... is this thing working?" << endl;
-	cout << "Updated the file for testing.";
+
 	cin.get();
 	return 0;
-}*/
+}
