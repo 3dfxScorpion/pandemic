1b543b0edce8a06f8622b40cc956ad40e1480da9
Scorpion <myScorpCode@gmail.com>
2013-10-21 17:18:29 -0700

    These files now work in Visual Studio. YAY! --Derrick

--- pandemicProject/codeCleanup/Map.h
+++ pandemicProject/codeCleanup/Map.h
@@ -6,14 +6,14 @@
 
 class Map {
   public:
-    Map();                    // default constructor
+    Map();                     // default constructor
 
-    int populateMap(string);  // populates map from file
-                              // (returns 0 if success, 1 if error)
-    int getNumberofCities();  // returns number of cities
+    int populateMap(string);   // populates map from file
+                               // (returns 0 if success, 1 if error)
+    int getNumberofCities();   // returns number of cities
     City* locateCity(string);  // takes in city name and returns City
-    string infectedList();    // returns formatted list of infected cities
-    string researchList();      // returns formatted list of cities housing research stations
+    string infectedList();     // returns formatted list of infected cities
+    string researchList();     // returns formatted list of cities housing research stations
 
     vector<City*> worldMap;
 };
