df81c13a2ad3b08564d0c4e8050181da6b36602e
dgoostree <darrengoostree@gmail.com>
2013-11-01 14:10:29 -0700

    Updated MVC, added main loop and infection. Added string getter to player

--- pandemicProject/mainProject/views/view.cpp
+++ pandemicProject/mainProject/views/view.cpp
@@ -8,3 +8,26 @@ void view::printTitle()
 	cout << "Set player count: ";
 }
 
+void view::displayPlayerInfo(string name, string role, string city)
+{
+	cout <<  "\n**Player: " << name <<  " Role: " << role << " Current Location: "  << city << "**" << endl;
+
+}
+
+void view::printCubeCount(int red, int yellow, int blue, int black)
+{
+	cout << "Cubes remaining:\n";
+	cout << "Red: " << red << " Yellow: " << yellow << " Blue: " << blue << " Black: " << black << endl;
+
+}
+
+void view::printAdj(vector<string>cities)
+{
+	cout << "\nAdjacent cities:\n";
+	while(!cities.empty())
+	{
+		cout << cities.front() << endl;
+		cities.erase(cities.begin());
+	}
+	cout << "Type a city's name to move: \n";
+}
